5990047f5913a79a2e3698ba8414d59194b1fccb
Sven A. Schmidt <sas@feinstruktur.com>
2015-07-01 14:01:29 +0100

    Added framework build target

---
+++ PocketSocket-Framework-OSX/PocketSocket.h
@@ -0,0 +1,22 @@
+//
+//  PocketSocket-Framework-OSX.h
+//  PocketSocket-Framework-OSX
+//
+//  Created by Sven Schmidt on 01/07/2015.
+//  Copyright © 2015 Zwopple Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PocketSocket-Framework-OSX.
+FOUNDATION_EXPORT double PocketSocket_Framework_OSXVersionNumber;
+
+//! Project version string for PocketSocket-Framework-OSX.
+FOUNDATION_EXPORT const unsigned char PocketSocket_Framework_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocketSocket_Framework_OSX/PublicHeader.h>
+
+#import "PSWebSocket.h"
+#import "PSWebSocketServer.h"
+#import "PSWebSocketDriver.h"
+#import "PSWebSocketTypes.h"
